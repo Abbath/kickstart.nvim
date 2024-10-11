69ef9c9f636dfc91cbe970ffc59cb3752a18291e
Abbath <pitongogi@gmail.com>
2024-10-11 09:25:10 +0200

    text case

--- init.lua
+++ init.lua
@@ -828,6 +828,30 @@ require('lazy').setup({
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp-signature-help',
     },
+    {
+      "johmsalas/text-case.nvim",
+      dependencies = { "nvim-telescope/telescope.nvim" },
+      config = function()
+        require("textcase").setup({})
+        require("telescope").load_extension("textcase")
+      end,
+      keys = {
+        "ga", -- Default invocation prefix
+        { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+      },
+      cmd = {
+        -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
+        "Subs",
+        "TextCaseOpenTelescope",
+        "TextCaseOpenTelescopeQuickChange",
+        "TextCaseOpenTelescopeLSPChange",
+        "TextCaseStartReplacingCommand",
+      },
+      -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+      -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+      -- available after the first executing of it or after a keymap of text-case.nvim has been used.
+      lazy = false,
+    }, 
     config = function()
       -- See `:help cmp`
       local cmp = require 'cmp'
