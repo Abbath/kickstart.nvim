bbaf5c069554375caa8af82da95e97af9ebd78fe
Abbath <pitongogi@gmail.com>
2024-10-19 14:33:25 +0200

    Add clangd everywhere

--- init.lua
+++ init.lua
@@ -624,6 +624,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -654,7 +655,6 @@ require('lazy').setup({
 
       if vim.loop.os_uname().sysname == 'Linux' then
         servers['hls'] = {}
-        servers['clangd'] = {}
       end
 
       -- Ensure the servers and tools above are installed
