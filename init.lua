cfe0f66206e5702e57eb1b28a2fd9c21c1a1adbc
Abbath <pitongogi@gmail.com>
2024-11-29 13:51:41 +0100

    Args keymap.

--- init.lua
+++ init.lua
@@ -472,6 +472,7 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>sa', ':lua require("telescope").extensions.live_grep_args.live_grep_args()<CR>', { desc = '[S]earch Live Grep [A]rgs' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
