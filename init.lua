c16f41ec63b4b881242da4d9c356180f927f7c30
Abbath <pitongogi@gmail.com>
2025-07-25 19:40:22 +0200

    Remove dropbar.

--- init.lua
+++ init.lua
@@ -1163,20 +1163,6 @@ require('lazy').setup({
     end,
   },
   { 'HiPhish/rainbow-delimiters.nvim' },
-  {
-    'Bekaboo/dropbar.nvim',
-    -- optional, but required for fuzzy finder support
-    dependencies = {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
-    },
-    config = function()
-      local dropbar_api = require 'dropbar.api'
-      vim.keymap.set('n', '<leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
-    end,
-  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
