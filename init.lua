75d5e11522c90eade48c664d71fc7f7a02e2ebd0
Abbath <pitongogi@gmail.com>
2025-09-12 14:18:56 +0200

    Switch header source keybinding for C/C++.

--- init.lua
+++ init.lua
@@ -254,6 +254,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'c', 'cpp', 'h', 'hpp' },
+  callback = function()
+    vim.keymap.set('n', '<F4>', ':LspClangdSwitchSourceHeader<CR>', { desc = 'Switch source/header' })
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
