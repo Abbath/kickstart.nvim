fdbbd644b1661bcd4a1c661622f77efc14e1b59e
Abbath <pitongogi@gmail.com>
2026-02-18 22:41:36 +0100

    Abolish.

--- init.lua
+++ init.lua
@@ -1336,6 +1336,7 @@ require('lazy').setup({
       { '<leader>n', '<cmd>Neogit<cr>', desc = 'Show Neogit UI' },
     },
   },
+  { 'tpope/vim-abolish' },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
