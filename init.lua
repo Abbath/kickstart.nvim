92af92fc946607c4bd29e32e70e52cb7921362b3
Abbath <pitongogi@gmail.com>
2026-06-25 17:37:49 +0200

    Sync some updates.

--- init.lua
+++ init.lua
@@ -598,7 +598,7 @@ do
     { '<leader><leader>', function() Snacks.picker.smart() end, desc = '[ ] Find existing buffers' },
     { '<leader>/', function() Snacks.picker.lines {} end, desc = '[/] Fuzzily search in current buffer' },
     { '<leader>s/', function() Snacks.picker.grep_buffers() end, desc = '[S]earch [/] in Open Files' },
-    { '<leader>sn', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, desc = '[S]earch [N]eovim files' },
+    { '<leader>sn', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config', follow = true } end, desc = '[S]earch [N]eovim files' },
     { '<leader>sj', function() Snacks.picker.jumps() end, desc = '[S]earch [J]umps' },
     {
       '<leader>p',
@@ -754,6 +754,7 @@ do
       },
     },
     ty = {},
+    -- zuban = {},
     ruff = {},
     hls = {
       settings = {
